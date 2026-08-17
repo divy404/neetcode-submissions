91e76bbb621fc702d80b198162b28b718f71352a
Divyansh Jain <146205222+divy404@users.noreply.github.com>
2026-08-17 22:39:29 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int>&nums, int i, int end,vector<int>&dp) {
+        if(i>end) {
+            return 0;
+        }
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int take = nums[i]+solve(nums,i+2,end,dp);
+        int skip = solve(nums,i+1,end,dp);
+
+        return dp[i] = max(take,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        if(n==1) {
+            return nums[0];
+        }
+        return max(solve(nums,0,n-2,dp1),solve(nums,1,n-1,dp2));
+    }
+};
