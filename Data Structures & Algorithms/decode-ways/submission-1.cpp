712dfaa40f2dfb5cbf858c5351142b8a9232609d
Divyansh Jain <146205222+divy404@users.noreply.github.com>
2026-08-19 23:08:39 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int>t(n+1,0);
+        t[n] = 1;
+
+        for(int i = n-1; i >=0; i--) {
+            if(s[i] == '0') {
+                t[i] = 0;
+            }else {
+                t[i] = t[i+1];
+            }
+
+            if(i+1 < n) {
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')) {
+                    t[i] += t[i+2];
+                }
+            }
+        }
+        return t[0];
+    }
+};
